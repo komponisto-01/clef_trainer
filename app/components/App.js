7d57e87eb4d09704cf9d1cf5d58f7351c4e4b437
komponisto <komponisto@yahoo.com>
2017-04-05 06:06:17 +0000

    saving

---
+++ app/components/App.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+function App(props){
+    return (
+        <h1>Hello, {props.addressee}!</h1>
+        );
+};
+
+module.exports = App;
---
+++ app/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('./components/App');
+
+ReactDOM.render(
+    <App addressee="world"/>,
+    document.getElementById('app')
+    );
